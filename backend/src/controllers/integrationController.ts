979f042ee7bce488fd75ba95938ab5ce6a3cb1f7
iamanandhuss <anandhustech1998@gmail.com>
2026-09-15 10:41:41 +0530

    fix: update google drive scopes to allow copying files

--- backend/src/controllers/integrationController.ts
+++ backend/src/controllers/integrationController.ts
@@ -15,7 +15,7 @@ const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_REDIRECT_URI
 );
 
-const SCOPES = ['https://www.googleapis.com/auth/drive.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
 
 export const getAuthUrl = async (req: AuthRequest, res: Response): Promise<void> => {
   try {
